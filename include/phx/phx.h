1d4576d2b08d6aff6a763344e47977a9cca56d76
aTxr <alexandre.tullot@gmail.com>
2022-01-12 18:54:19 +0100

    Clean unused dep and typedef moved to lib/lib.h

--- include/phx/phx.h
+++ include/phx/phx.h
@@ -1,7 +1,7 @@
 #ifndef PHX_PHX_H
 #define PHX_PHX_H
 
-#include <tuple>
+#include "lib/lib.h"
 #include <vector>
 #include <iostream>
 #include "entity/strategies/physical_entity.h"
@@ -9,31 +9,11 @@
 // Constrain
 // vitessePersoMax < longeurPersoMin
 
-// dependencies
-class Scene;
-
-typedef std::pair<unsigned int, unsigned int> Vec;
-typedef std::pair<int, int> Vel;
-
-/** 
- * Update each Entity in the Scene
- */
-void update(Scene);
-
 Vel updateCinematics(PhysicalEntity const);
-void updateCollision(Scene);
-
-class Cinematics {
-private:
-    Vec m_pos;
-    Vel m_vel;
-};
 
 class Hitbox {
 private:
     std::vector<Vec> m_vertices;
 };
-
-enum Force {weight};
 
 #endif
